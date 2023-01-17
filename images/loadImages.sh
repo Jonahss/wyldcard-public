563cac5915e87e747f53f200efcb7a2ff7cf947a
Jonah Stiennon <jonahss@gmail.com>
2023-01-17 10:29:20 -0800

    move setup file to private

--- images/loadImages.sh
+++ images/loadImages.sh
@@ -1,8 +1,12 @@
-ssh pi@raspberrypi.local "rm -rf /home/pi/Pictures/wyldcard"
-ssh pi@raspberrypi2.local "rm -rf /home/pi/Pictures/wyldcard"
+# ssh pi@raspberrypi.local "rm -rf /home/pi/Pictures/wyldcard"
+# ssh pi@raspberrypi2.local "rm -rf /home/pi/Pictures/wyldcard"
 
-ssh pi@raspberrypi.local "mkdir -p /home/pi/Pictures/wyldcard"
-ssh pi@raspberrypi2.local "mkdir -p /home/pi/Pictures/wyldcard"
+# ssh pi@raspberrypi.local "mkdir -p /home/pi/Pictures/wyldcard"
+# ssh pi@raspberrypi2.local "mkdir -p /home/pi/Pictures/wyldcard"
 
-scp -r ../../../art/converted/collectionB pi@raspberrypi.local:/home/pi/Pictures/wyldcard/
-scp -r ../../../art/converted/collectionB pi@raspberrypi2.local:/home/pi/Pictures/wyldcard/
+# scp -r ../../../art/converted/collectionB pi@raspberrypi.local:/home/pi/Pictures/wyldcard/
+# scp -r ../../../art/converted/collectionB pi@raspberrypi2.local:/home/pi/Pictures/wyldcard/
+
+ssh pi@raspberrypizero.local "rm -rf /home/pi/Pictures/wyldcard"
+ssh pi@raspberrypizero.local "mkdir -p /home/pi/Pictures/wyldcard"
+scp -r ../../../art/converted/collectionB pi@raspberrypizero.local:/home/pi/Pictures/wyldcard
