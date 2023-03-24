cec31a16097a2a27500b1dd35543b70368d8f020
jonahss <jonahss@gmail.com>
2023-03-24 15:18:18 -0700

    test.js file was being confused as an official unit test. also unit test was broken because I removed that sum function. come back to testing later. updating deps seems to have fixed the build

--- napi-rust-drivers/demo.js
+++ napi-rust-drivers/demo.js
@@ -1,7 +1,7 @@
 let fs = require('fs/promises')
 let path = require('path')
 
-let { Plinth, imageUtilities } = require('./')
+let { Plinth, imageUtilities } = require('.')
 
 async function main() {
   let plinth = new Plinth('prototype')
